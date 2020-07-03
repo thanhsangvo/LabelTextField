6bf4a75ad54821499d37405c4692b29e62deb7aa
thanhsangvo <pl.thanhsang0710@gmail.com>
2020-07-03 23:42:38 +0700

    Initial Commit

---
+++ LabelTextField/LabelTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabelTextFieldApp.swift
+//  LabelTextField
+//
+//  Created by Võ Thanh Sang on 7/3/20.
+//
+
+import SwiftUI
+
+@main
+struct LabelTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
